ab72197273ebffb8c23627c2e44f3b1da18708ab
bpatil-git <bpatil462@gmail.com>
2020-09-20 14:51:09 +0530

    space added

--- module1/views.py
+++ module1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages, sessions
 
+
 # Create your views here.
 
 def landing_page(request):
